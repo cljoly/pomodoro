79721fe2430707e534f0a394d00075f156866cc4
Leo <leowzukw@vmail.me>
2016-09-10 15:44:52 +0200

    Create example log file

---
+++ example_log.scm
@@ -0,0 +1,13 @@
+(
+ (settings(
+  (pomodoro_duration 25)
+  (short_break_duration 5)
+  (long_break_duration 30)
+ ))
+ (tasks
+  ;; (((name "Task name") (description "Task description")
+  ;;   (done_at ("2016-09-10T15:42:38")) (done_with (5)))
+   ((name French) (description "Learn it!") (done_at ()) (done_with ())))
+ )
+)
+
