29d17a2773e6d5a19104354726ad385b127166a6
Leo <leowzukw@vmail.me>
2016-12-22 09:54:45 +0100

    Add more log file to test
     + Add an empty and a big one
     + Correct space inconsistencies in dev_log

---
+++ empty_log.scm
@@ -0,0 +1,12 @@
+(
+ (settings(
+  (pomodoro_duration 0.2)
+  (short_break_duration 0.1)
+  (long_break_duration 0.15)
+  (ticking_command "ogg123 ~/clock_ticking.ogv &>/dev/null")
+  (ringing_command "ogg123 ~/bell_sound.ogv &>/dev/null")
+ ))
+ (tasks(
+ ))
+)
+
