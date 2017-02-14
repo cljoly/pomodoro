cljoly/pomodoro

commit a3fca9aed16877c8e580e8508fa8da6f6a85595f

Attaching timer to task is a non-sens
 + The cycle Pomodoro-Break-... should be independent of task, it's done now.
 + Use type in log_f to carry information and less to create objects, even
 though task object continue to be created.
 + Annotate type of long since not used but keep to prepare different
 logging of long short interruption in log file
 + More changes prepared and coming. Some are visible in TODO.

src/timer.ml
@@ -44,6 +44,13 @@ module Ts = Time.Span;;
 type of_timer =
     Pomodoro | Short_break | Long_break
 ;;
+(* Return durations associated in log file for each type of timer *)
+let durations_and_name log =
+  function
+    | Pomodoro -> (log.Log_f.settings.pomodoro_duration, "Pomodoro")
+    | Short_break -> (log.Log_f.settings.short_break_duration, "Short break")
+    | Long_break -> (log.Log_f.settings.long_break_duration, "Long break")
+;;
 
 (* When a timer is finished, notify *)
 let on_finish timer =
@@ -133,6 +140,45 @@ class timer duration of_type ~on_finish name running_meanwhile running_when_done
 (* TODO Create a special object for timer that are elleapsed and that do nothing *)
 let empty_timer () =
   new timer 0. Short_break ~on_finish:(fun _ -> ()) "Empty" "" ""
-  |> Option.some
 ;;
 
+class cycling ?cycle ~log =
+  (* Simplified instanciation of class timer *)
+  let simple_timer of_timer =
+    let ticking_command = !log.Log_f.settings.ticking_command in
+    let ringing_command = !log.Log_f.settings.ringing_command in
+    let (duration, name) = durations_and_name !log of_timer in
+    new timer duration of_timer ~on_finish:on_finish name ticking_command ringing_command
+  in
+  (* We do 4 pomodoroes, with a short break between each, before taking a long
+   * break *)
+  let cycle = (* TODO Allow to configure this *)
+    Option.value cycle
+      ~default:[ Pomodoro ; Short_break
+               ; Pomodoro ; Short_break
+               ; Pomodoro ; Short_break
+               ; Pomodoro ; Long_break
+               ]
+  in
+  (* lead to problem if cycle is empty, for instance with `position` instance variable *)
+  let _ = assert (cycle <> []) in
+  let cycle_length = List.length cycle in
+  object (s:'s)
+    val cycle : of_timer list Avl.t = new Avl.t cycle
+    val cycle_length = cycle_length
+    val mutable position = -1
+    val mutable current_timer = empty_timer ()
+    (* Circle through positions *)
+    method private next_position =
+      position <- (position + 1) mod cycle_length;
+      current_timer <- simple_timer (List.nth_exn cycle#get position)
+    method get final_call =
+      if current_timer#is_finished then begin
+        final_call current_timer;
+        s#next_position
+      end;
+      current_timer
+    method map_current_timer ~f =
+      current_timer <- f current_timer
+  end
+;;

src/timer.mli
@@ -67,6 +67,17 @@ class timer :
     method update_running_meanwhile : unit
   end
 
-val empty_timer : unit -> timer option
-
 val on_finish : timer -> unit
+
+(* A cycling set of timers, like pomodro, break, pomodoro, break, pomodoro, long
+ * break *)
+class cycling : ?cycle:(of_timer list) -> log:(Log_f.read_log ref) ->
+  object ('a)
+    (* Return current timer. Cycles through timers and call final_call, as one finishes *)
+    method get : (timer -> unit) -> timer
+    (* Change current timer. May change a finished, which would be deleted as
+     * get method is called. *)
+    method map_current_timer : f:(timer -> timer) -> unit
+  end
+;;
+

src/log_f.ml
@@ -38,7 +38,7 @@ open Core.Std;;
 (* Tools with log file *)
 
 (* Simple log of pomodoros & tasks, with settings *)
-type settings = {
+type settings_sexp = {
   (* Defaults from pomodoro guide *)
   pomodoro_duration : float;
   short_break_duration : float;
@@ -59,12 +59,12 @@ type task_sexp = {
   day : Date.t sexp_option (* The day you plan to do the task *)
 } [@@deriving sexp]
 type log = {
-  settings : settings;
+  settings : settings_sexp;
   tasks : task_sexp list
 } [@@deriving sexp]
 
 (* fname stands for filename *)
-type read_log = { fname : string ; log : Tasks.ptask list }
+type read_log = { fname : string ; settings : settings_sexp ; ptasks : Tasks.ptask list }
 
 (* Read log containing tasks and settings, tries multiple times since user may
  * edit file and lead to temporal removal *)
@@ -78,29 +78,10 @@ let read_log filename =
       else raise exn
   in
   let log = read_log 30 in
-  let durations = [
-    ( Timer.Pomodoro, (log.settings.pomodoro_duration, "Pomodoro") );
-    ( Timer.Short_break, (log.settings.short_break_duration, "Short break") );
-    ( Timer.Long_break, (log.settings.long_break_duration, "Long break") )
-  ] in
-  let simple_timer of_timer = (* Simplified instanciation of class timer *)
-    let ticking_command = log.settings.ticking_command in
-    let ringing_command = log.settings.ringing_command in
-    let (duration, name) = List.Assoc.find_exn durations of_timer in
-    new Timer.timer duration of_timer ~on_finish:Timer.on_finish name ticking_command ringing_command
-  in
-  (* We do 4 pomodoroes, with a short break between each, before taking a long
-   * break *)
-  let cycle = (* TODO Allow to configure this *)
-    [ Timer.Pomodoro ; Timer.Short_break
-    ; Timer.Pomodoro ; Timer.Short_break
-    ; Timer.Pomodoro ; Timer.Short_break
-    ; Timer.Pomodoro ; Timer.Long_break
-    ]
-  in
   {
     fname = filename;
-    log = List.mapi log.tasks
+    settings = log.settings;
+    ptasks = List.mapi log.tasks
         ~f:(fun task_position (task_sexp:task_sexp) ->
             new Tasks.ptask
               ~num:task_position
@@ -111,8 +92,6 @@ let read_log filename =
               ?estimation:task_sexp.estimation
               ?interruption:task_sexp.interruption
               ?day:task_sexp.day
-              cycle
-              simple_timer
           );
   }
 
@@ -121,22 +100,14 @@ let read_log filename =
  * one. Makes sure we stop timers of task going deeper in the list *)
 let reread_log r_log =
   (* Disable timer from tasks other than the first, active, one *)
-  let disable_all_but_first ptasks =
-    List.filter ~f:(fun ptask -> not ptask#is_done) ptasks
-    |> (function
-        | [] -> ()
-        | (_ :: task2stop) ->
-          List.iter ~f:(fun timer -> timer#remove_timer) task2stop;
-      );
-    ptasks
-  in
   let fname = r_log.fname in (* Name is common to both logs *)
-  let old_log = r_log.log in
-  let new_log = (read_log fname).log in
-  let log = (* Merge current state and log file *)
-    List.map new_log
+  let new_log = (read_log fname) in
+  let old_ptasks = r_log.ptasks in
+  let new_ptasks = (read_log fname).ptasks in
+  let ptasks = (* Merge current state and log file *)
+    List.map new_ptasks
       ~f:(fun new_task ->
-          List.find_map old_log
+          List.find_map old_ptasks
             ~f:(fun old_task ->
                 if new_task#id = old_task#id
                 then Some (old_task#update_with new_task)
@@ -144,7 +115,7 @@ let reread_log r_log =
               )
           |> Option.value ~default:new_task
         )
-    |> disable_all_but_first
   in
-  { fname ; log }
+  (* Erase old settings TODO merge old and new *)
+  { fname ; ptasks ; settings = new_log.settings }
 ;;

src/log_f.mli
@@ -37,6 +37,20 @@ Inspired by example of lambda-term library, (c) 2011, Jeremie Dimino <jeremie@di
 
 open Core.Std;;
 
-type read_log = { fname : string; log : Tasks.ptask list }
+type settings_sexp = {
+  (* Defaults from pomodoro guide *)
+  pomodoro_duration : float;
+  short_break_duration : float;
+  long_break_duration : float;
+  (* Command to play sound while pomodoro is running *)
+  ticking_command : string;
+  (* Command to play sound when pomodoro is finished *)
+  ringing_command : string;
+}
+type read_log = {
+    fname : string;
+    settings : settings_sexp;
+    ptasks : Tasks.ptask list;
+}
 val read_log : string -> read_log
 val reread_log : read_log -> read_log

src/pomodoro.ml
@@ -37,7 +37,7 @@ open Core.Std;;
 
 let () =
   (* Get timers with command line arguments, mutable to allow easier update *)
-  let ptasks = ref (
+  let log = ref (
       Sys.argv |> function
       | [| _ ; name |] -> Log_f.read_log name
       | _ -> failwith "Needs exactly one argument, filename of your log file."
@@ -45,8 +45,8 @@ let () =
   (* Update log file *)
   (Lwt_engine.on_timer Param.log_tick true
      (fun _ ->
-        ptasks := Log_f.reread_log !ptasks))
+        log := Log_f.reread_log !log))
   |> ignore;
 
-  Lwt_main.run (Views.mainv ~ptasks ())
+  Lwt_main.run (Views.mainv ~log ())
 ;;

src/tasks.ml
@@ -53,10 +53,7 @@ class ptask
     ?estimation
     ?interruption
     ?day
-    cycle
-    (simple_timer:(Timer.of_timer -> Timer.timer))
   =
-  let cycle_length = List.length cycle in
   object(s:'s)
     val name : string Avl.t = new Avl.t name
     val description : string Avl.t = new Avl.t description
@@ -79,22 +76,20 @@ class ptask
     val num : int option Avl.t = new Avl.t num
     method num = num
 
-    val cycle : Timer.of_timer list Avl.t = new Avl.t cycle
-    val cycle_length = cycle_length
-    (* Position in the cycle, lead to problem if cycle is empty *)
-    val mutable position = -1
-    val mutable current_timer = None
     val number_of_pomodoro : int option Avl.t = new Avl.t number_of_pomodoro
+    (* Record one more pomodoro *)
+    method record_pomodoro =
+      number_of_pomodoro#set
+        (Option.value ~default:0 number_of_pomodoro#get
+         |> (fun nop -> nop + 1)
+         |> Option.some);
 
     val interruption : int option Avl.t = new Avl.t interruption
     method interruption = interruption
     (* Record an interruption which reset timer if its too long *)
-    method record_interruption ~long =
+    method record_interruption ~(long:bool) =
       interruption#set
         (Some (Option.value_map ~default:(0+1) ~f:succ interruption#get));
-      if long
-      then
-        current_timer <- Option.map current_timer ~f:(fun ct -> ct#reset);
 
     val day : Date.t option Avl.t = new Avl.t day
     method day = day
@@ -103,38 +98,6 @@ class ptask
     method estimation = estimation
 
     method number_of_pomodoro = number_of_pomodoro
-    (* Return current timer. Cycles through timers, as one finishes *)
-    method current_timer () =
-      let is_some_finished =
-        Option.value_map ~default:false
-          ~f:(fun ct -> ct#is_finished)
-      in
-      if
-        status#get = Active
-        && is_some_finished current_timer
-      then begin
-        let ct = Option.value_exn current_timer in
-        if ct#of_type = Timer.Pomodoro
-        then
-          number_of_pomodoro#set
-            (Option.value ~default:0 number_of_pomodoro#get
-             |> (fun nop -> nop + 1)
-             |> Option.some);
-        (* Circle through positions *)
-        position <- (position + 1) mod cycle_length;
-        current_timer <- Some (simple_timer (List.nth_exn cycle#get position));
-      end
-      ;
-      current_timer
-    (* To interrupt a task *)
-    method remove_timer = current_timer <- None
-    (* Attach a timer to a task *)
-    method attach_timer =
-      if Option.is_none current_timer
-      then begin
-        status#set Active;
-        current_timer <- Timer.empty_timer ()
-      end
 
     (* Returns a summary of the task, short or with more details *)
     method private summary ~long =

src/tasks.mli
@@ -48,10 +48,7 @@ class ptask :
   ?estimation:int ->
   ?interruption:int ->
   ?day:Date.t ->
-  Timer.of_timer list ->
-  (Timer.of_timer -> Timer.timer) ->
   object ('a)
-    method current_timer : unit -> Timer.timer option
     method description : string Avl.t
     method done_at : string option Avl.t
     method estimation : int option Avl.t
@@ -60,15 +57,16 @@ class ptask :
     method id : int
 
     method record_interruption : long:bool -> unit
-    method remove_timer : unit
-    method attach_timer : unit
 
     method is_done : bool
     method long_summary : string
     method mark_done : unit
     method name : string Avl.t
     method num : int option Avl.t
+
     method number_of_pomodoro : int option Avl.t
+    method record_pomodoro : unit
+
     method short_summary : string
     method status : status Avl.t
     method private summary : long:bool -> string

src/views.ml
@@ -44,12 +44,12 @@ open LTerm_geom;;
 (* A view with both task and pomodoro timers *)
 
 (* Scrollable list of tasks *)
-class scrollable_task_list ~ptasks (scroll : scrollable) display_task =
-  let log () = !ptasks.Log_f.log in
+class scrollable_task_list ~log (scroll : scrollable) display_task =
+  let ptasks () = !log.Log_f.ptasks in
   object
     inherit t "task_list"
 
-    initializer scroll#set_range (List.length (log ()))
+    initializer scroll#set_range (List.length (ptasks ()))
 
     method! can_focus = false
 
@@ -99,7 +99,7 @@ class scrollable_task_list ~ptasks (scroll : scrollable) display_task =
           task_list
       in
       let offset = scroll#offset in
-      log ()
+      ptasks ()
       |> List.filter ~f:display_task
       |> select_task offset
       |> draw_table_of_task
@@ -107,31 +107,37 @@ class scrollable_task_list ~ptasks (scroll : scrollable) display_task =
   end;;
 
 (* Place scrollable task list *)
-let add_scroll_task_list ~ptasks (box : box) display_task =
+let add_scroll_task_list ~log (box : box) display_task =
   let adj = new scrollable in
   let scroll = new vscrollbar adj in
-  let task_list = new scrollable_task_list ~ptasks adj display_task in
+  let task_list = new scrollable_task_list ~log adj display_task in
   box#add ~expand:true task_list;
   box#add ~expand:false scroll;
   adj#on_offset_change (fun _ -> scroll#queue_draw);
   (task_list, scroll, adj)
 ;;
 
-(* Place pomodoro timer *)
-let add_pomodoro_timer ~ptasks (box:box) =
+(* Place the pomodoro timer *)
+let add_pomodoro_timer ~log (box:box) =
   let current_task ~default f =
-    Tasks.get_pending !ptasks.Log_f.log
+    Tasks.get_pending !log.Log_f.ptasks
     |> Option.value_map ~f ~default
   in
-  (* Allow to get remaining time for current ptask, if any one is yet active *)
+  let timer_cycle : Timer.cycling =
+    new Timer.cycling ~log ?cycle:None
+  in
+  (* Allow to get remaining time *)
   let remaining_time () =
-    let rec timer_of_current_task ptask =
-      match ptask#current_timer () with
-      | None -> ptask#attach_timer; timer_of_current_task ptask
-      | Some timer ->
-        String.concat [ timer#name ; "\n" ; timer#remaining_str ]
+    let timer =
+      timer_cycle#get
+          (fun timer ->
+             match timer#of_type with
+           | Pomodoro -> current_task ~default:()
+                           (fun task -> task#record_pomodoro)
+           | _ -> ()
+        )
     in
-    current_task ~default:"Finished" timer_of_current_task
+    String.concat [ timer#name ; "\n" ; timer#remaining_str ]
   in
   let task_summary () =
     current_task ~default:"" (fun ptask -> ptask#long_summary)
@@ -148,12 +154,6 @@ let add_pomodoro_timer ~ptasks (box:box) =
   (* Update displayed time on every tick *)
   (Lwt_engine.on_timer Param.tick true
      (fun _ ->
-        (* Make sure timer is in a consistent state *)
-        current_task ~default:()
-          (fun ct -> ct#current_timer ()
-                     |> Option.iter
-                       ~f:(fun ct -> ct#update_running_meanwhile));
-        (* Update display *)
         clock#set_text (remaining_time ());
         ptask#set_text (task_summary ());
      ))
@@ -161,7 +161,10 @@ let add_pomodoro_timer ~ptasks (box:box) =
 
   (* Record interruptions *)
   linter_btn#on_click
-    (fun () -> current_task ~default:() (fun t -> t#record_interruption ~long:true));
+    (fun () -> current_task ~default:()
+      (fun t ->
+        t#record_interruption ~long:true;
+        timer_cycle#map_current_timer (fun timer -> timer#reset)));
   sinter_btn#on_click
     (fun () -> current_task ~default:() (fun t -> t#record_interruption ~long:false));
 
@@ -209,7 +212,7 @@ let add_bottom_btn ~(main:vbox) ~(adj:scrollable) ~wakener display_done_task dis
 ;;
 
 (* Main view *)
-let mainv ~ptasks () =
+let mainv ~log () =
   let waiter, wakener = wait () in
   let main = new vbox in
   let display_done_task = ref false in
@@ -226,9 +229,9 @@ let mainv ~ptasks () =
     )
   in
 
-  add_pomodoro_timer ~ptasks main;
+  add_pomodoro_timer ~log main;
   main#add ~expand:false (new hline);
-  let ( _, _, adj) = add_scroll_task_list ~ptasks main display_task in
+  let ( _, _, adj) = add_scroll_task_list ~log main display_task in
   main#add ~expand:false (new hline);
   add_bottom_btn ~main ~adj ~wakener display_done_task display_today_only;
 

commit 0d674942c023d6e46b4612a093e7b05cc639c2fa

Make sure to return timers in a consistent state
 + Without this, the ticking command may not be running.
 + Bug introduced in commit a3fca9aed16877c8e580e8508fa8da6f6a85595f
 (not reused the lines with the comment “Make sure the timer is in a
 consistent state” and call to method update_running_meanwhile)

src/timer.ml
@@ -119,7 +119,7 @@ class timer duration of_type ~on_finish name running_meanwhile ?max_done_duratio
      * instanciation *)
     val mutable running_meanwhile = run_meanwhile ()
     (* Stop and keep running when necessary *)
-    method update_running_meanwhile =
+    method ensure_consistency =
       let make_sure_its_running () =
         running_meanwhile#state
         |> function | Lwt_process.Running -> ()
@@ -185,8 +185,10 @@ class cycling ?cycle ~log =
         final_call current_timer;
         s#next_position
       end;
+      current_timer#ensure_consistency;
       current_timer
     method map_current_timer ~f =
-      current_timer <- f current_timer
+      current_timer <- f current_timer;
+      current_timer#ensure_consistency;
   end
 ;;

src/timer.mli
@@ -65,7 +65,7 @@ class timer :
     method remaining : Ts.t option
     method remaining_str : string
     method run_done : unit
-    method update_running_meanwhile : unit
+    method ensure_consistency : unit
   end
 
 val on_finish : timer -> unit